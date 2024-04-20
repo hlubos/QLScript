f6c1aaa36c38a84cb109e1f8b2fe791c50b44842
hlubos <112747087+hlubos@users.noreply.github.com>
2024-04-20 21:38:00 +0800

    阿里云盘

--- aliYun.js
+++ aliYun.js
@@ -1,6 +1,7 @@
 //阿里云盘连续签到活动
 //https://alist.nn.ci/zh/guide/drivers/aliyundrive.html 打开页面扫码获取refresh_token
 //环境变量:ALI_TOKEN,多账号用换行或@或&分隔
+// const $ = new Env('阿里云盘')
 
 const $ = API();
 let refresh_token = [];
